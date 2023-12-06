69de9dc8ebed1ef8078f85a9ddee6faa87203a1b
Shane Celis <shane.celis@gmail.com>
2023-12-06 00:04:06 -0500

    feature: Futzin' around.

--- src/bin/day5p2.rs
+++ src/bin/day5p2.rs
@@ -23,6 +23,43 @@ struct MapRange {
     length: u64,
 }
 
+#[derive(Debug)]
+struct CompositeRange {
+    ranges: Vec<Range<u64>>
+}
+
+impl CompositeRange {
+
+    fn select_mut<'a>(&'a mut self, other: &Range<u64>) -> Vec<usize> {
+        let mut result: Vec<usize> = Vec::new();
+        for i in 0..self.ranges.len() {
+            match self.ranges[i].intersect_ext(other) {
+                Empty | Less | Greater => {},
+                Same | Over => result.push(i),
+                LessOverlap => {
+                    self.ranges.insert(i + 1, Range { start: other.start, end: self.ranges[i].end });
+                    self.ranges[i].end = other.start - 1;
+                    result.push(i + 1);
+                }
+                GreaterOverlap => {
+                    self.ranges.insert(i + 1, Range { start: other.end, end: self.ranges[i].end });
+                    self.ranges[i].end = other.end;
+                    result.push(i);
+                }
+                Within => {
+                    let end = self.ranges[i].end;
+                    self.ranges[i].end = other.start - 1;
+                    self.ranges.insert(i + 1, other);
+                    self.ranges.insert(i + 2, other);
+
+
+                }
+            }
+        }
+        result
+    }
+}
+
 impl std::str::FromStr for Map {
     type Err = String;
 
@@ -47,19 +84,30 @@ fn seeds(input: &mut &str) -> PResult<Vec<u64>> {
 }
 
 impl MapRange {
-    fn range(&self) -> Range<u64> {
+    fn source_range(&self) -> Range<u64> {
         self.source_start..(self.source_start + self.length)
     }
 
-    fn map_range(&self, source: &Range<u64>) -> Option<Range<u64>> {
-        let r = self.range();
+    fn dest_range(&self) -> Range<u64> {
+        self.dest_start..(self.dest_start + self.length)
+    }
+
+    fn covers_source(&self, source: &Range<u64>) -> Option<Range<u64>> {
+        let r = self.source_range();
         match r.intersect_ext(source) {
             Empty | Less | Greater | Same => None,
-            Over => Some(Range { start: self.map(source.start).unwrap(), end: self.map(source.end).unwrap() }),
-            LessOverlap => Some(Range { start: self.map(source.start).unwrap(), end: r.end }),
-            GreaterOverlap => Some(Range { start: r.start, end: self.map(source.end).unwrap() }),
+            Over => Some(Range { start: source.start, end: source.end }),
+            LessOverlap => Some(Range { start: source.start, end: r.end }),
+            GreaterOverlap => Some(Range { start: r.start, end: source.end }),
             Within => Some(r),
         }
+    }
+
+    fn map_range(&self, source: &Range<u64>) -> Option<Range<u64>> {
+        let source_map_maybe = self.covers_source(source);
+        source_map_maybe.map(|s| Range { start: s.start - self.source_start + self.dest_start,
+                                         end: s.end - self.source_start + self.dest_start })
+
     }
 
     fn map(&self, source: u64) -> Option<u64> {
@@ -73,6 +121,10 @@ impl MapRange {
 }
 
 impl Map {
+    fn sort(&mut self) {
+        self.ranges.sort_by(|a, b| a.source_start.cmp(&b.source_start));
+    }
+
     fn map(&self, source: u64) -> u64 {
         for map_range in &self.ranges {
             if let Some(x) = map_range.map(source) {
@@ -82,8 +134,17 @@ impl Map {
         return source;
     }
 
-
     fn map_range<'a>(&'a self, source: &'a Range<u64>) -> impl Iterator<Item = Range<u64>> + 'a {
+        let mut result: Vec<Range<u64>> = Vec::new();
+        let mut start = source.start;
+        let mut end = source.end;
+        for range in &self.ranges {
+            if let Some(source_range) = range.covers_source(source) {
+                result.push(Range { start: start, end: source_range.start });
+
+            }
+
+        }
         self.ranges.iter().filter_map(|r| r.map_range(source))
     }
 }
@@ -127,10 +188,29 @@ fn number_list(input: &mut &str) -> PResult<Vec<u64>> {
         .parse_next(input)
 }
 
+fn eval_maps(seed: u64, maps: &Vec<Map>) -> u64 {
+    let mut value: u64 = seed;
+    for map in maps {
+        value = map.map(value);
+    }
+    value
+}
+
+fn find_range_maps(seed: Range<u64>, maps: &Vec<Map>) -> impl Iterator<Item = Range<u64>> {
+    let mut value: Vec<Range<u64>> = vec![seed];
+    for map in maps {
+        let mut new_value: Vec<Range<u64>> = Vec::new();
+        for v in &value {
+            new_value.extend(map.map_range(v));
+        }
+        value.extend(new_value);
+    }
+    value.into_iter()
+}
 
 fn main() {
     let args: Vec<String> = env::args().collect();
-    let mut maps: HashMap<String, Map> = HashMap::new();
+    let mut maps: Vec<Map> = Vec::new();
     let mut current_map: Option<Map> = None;
     if let Ok(mut lines) = read_lines(&args[1]) {
         let first_line: String = lines.next().unwrap().unwrap();
@@ -140,7 +220,7 @@ fn main() {
             if let Ok(l) = line {
                 if l.trim().len() == 0 {
                     if let Some(map) = current_map.take() {
-                        maps.insert(map.from.clone(), map);
+                        maps.push(map);
                     }
                 } else {
                     match current_map {
@@ -151,29 +231,16 @@ fn main() {
             }
         }
         if let Some(map) = current_map.take() {
-            maps.insert(map.from.clone(), map);
+            maps.push(map);
+        }
+        for map in &mut maps {
+            map.sort();
         }
         let mut lowest_location: u64 = u64::MAX;
         for i in 0..(seeds.len()/2) {
             for seed in seeds[i*2]..(seeds[i*2] + seeds[i*2+1]) {
-                let mut value: u64 = seed;
-                let mut source = String::from("seed");
-
-                loop {
-                    // print!("{source} {value}, ");
-                    match maps.get(&source) {
-                        None => {
-                            lowest_location = value.min(lowest_location);
-                            // println!("");
-                            break;
-                        },
-                        Some(map) => {
-                            value = map.map(value);
-                            source.clear();
-                            source.push_str(map.to.as_str());
-                        }
-                    }
-                }
+                let value: u64 = eval_maps(seed, &maps);
+                lowest_location = value.min(lowest_location);
             }
         }
         println!("lowest_location {lowest_location}");
@@ -226,7 +293,9 @@ mod tests {
     fn test_map_range_check_extreme() {
         let range = MapRange { dest_start: 52, source_start: 50, length: 48 };
         assert_eq!(range.map_range(&(0..10)), None);
-        assert_eq!(range.map_range(&(0..60)), Some(50..62));
+        assert_eq!(range.map_range(&(0..60)), Some(52..62));
         assert_eq!(range.map_range(&(60..65)), Some(62..67));
+        assert_eq!(range.map_range(&(0..200)), Some(52..100));
+        assert_eq!(range.map_range(&(200..300)), None);
     }
 }
